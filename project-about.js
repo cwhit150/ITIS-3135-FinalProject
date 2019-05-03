dc3ff5e805d4b5a75a8df19f65b820124f77b753
Colton White <cwhit150@uncc.edu>
2019-05-02 22:25:41 -0400

    HW5 submission

--- project-about.js
+++ project-about.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-    $("#tabs").tabs().accordion();
+  /* 
+   * Ideally, I would use AJAX to populate just the officers section
+   * with a picture and short bio from each officer; this will be
+   * added in the future if we start using the site.
+   */
+  $.ajax({
+    type: "get",
+    url: "about.json",
+    timeout: 10000,
+    error: function(xhr, status, error) {
+      alert("Error: " + xhr.status + " - " + error);
+    },
+    dataType: "json",
+    success: function(data) {
+      $.each(data, function() {
+        $.each(this, function(key, value) {
+          $("#tabs-3").html("<p>" + value.text + "</p>");
+        });
+      });
+    }
+  });
+  $("#tabs").tabs().accordion();
 });
